a7941566f72eaadf8340ed9a920c80e12b3445e1
Giraud <fazzeur.white@gmail.com>
2022-09-23 13:49:37 +0200

    card and parameter first

--- function.php
+++ function.php
@@ -9,35 +9,39 @@ include('config/db_connect.php');
 
 //test des fonction ici
 
+//fonction d'inscription
 //register($bdd, "baptiste.giraud@epitech.eu", "giraud", "baptiste", "azerty83", "baba");
 function function_register($bdd, $emailuser, $nomuser, $prenomuser, $mdpuser, $usernameuser){
     register($bdd, $emailuser, $nomuser, $prenomuser, $mdpuser, $usernameuser);
 
 }
 
+//fonction de connexion
 //connexion($bdd, "baptiste.giraud@epitech.eu", "azerty83");
 function function_connexion($bdd, $email, $mdpenter){
     connexion($bdd, $email, $mdpenter);
 
 }
 
+//fonction parametre de la page
 //update_parameter_template($bdd, "ttt", "aa", "dd", "sss", "dd", "dqqd", "0");
 function function_update_parameter_template($bdd, $type_composition, $template_url, $color_page, $description, $texte_color, $police, $views_count){
     update_parameter_template($bdd, $type_composition, $template_url, $color_page, $description, $texte_color, $police, $views_count);
 
 }
 
+//fonction insert lien
 //insertcard($bdd, 1, "type", "effect", "url", "color_card", "texte");
 function function_insertcard($bdd, $forme, $type, $effect, $url, $color_card, $texte){
     insertcard($bdd, $forme, $type, $effect, $url, $color_card, $texte);
 }
-
+//fonction get lien par rapport a l'utilisateur connecter
 //selectcard($bdd);
 function function_selectcard($bdd){
     return(selectcard($bdd));
 }
 
-
+//fonction get lien par rapport a l'id
 //selectcardbyuserid($bdd, 3);
 function function_selectcardbyuserid($bdd, $id){
    return( selectcardbyuserid($bdd, $id));
