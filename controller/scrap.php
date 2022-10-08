714372d39eb8b69daef2ec43ff5df2a0fe15ac62
Giraud <fazzeur.white@gmail.com>
2022-10-08 14:21:56 +0200

    check url scrap

--- controller/scrap.php
+++ controller/scrap.php
@@ -3,12 +3,29 @@
 
  include('simple_html_dom.php');
 
+// $val = scrap('https://beacons.ai/lola83');
+// var_dump($val);
+ function scrap($url){
+    $value = parse_url($url);
+    if($value["host"] == "linktr.ee"){
+        return(scrap_linktree($url));
+    }else if ($value["host"] == "beacons.ai"){
+        echo 'baecons';
+
+    }else if($value["host"] == "snipfeed.co"){
+        echo 'snip';
+    }else{
+        http_response_code(500);
+    }
+ } 
+
+
+
  function scrap_linktree($url){
 
  $html = file_get_html($url);
 
  $img = $html->find('.Header__StyledProfilePicture-sc-x93kmr-1');
- 
  $array =  (array) $img;
  $img = $array[0]->attr['src'];
  $list = $html->find('<div',0);
@@ -16,8 +33,13 @@
  $list_array = $list->find('a');
  $array = [];
  foreach ( $list_array as $key=>$element ){
-    $array[$key]['text'] = remplacerSautDeLigne($element->plaintext);
-    $array[$key]['url'] = $url = $element->href;
+    $val = stripos($url = $element->href, "http");
+    if($val === false){
+       
+    }else{
+        $array[$key]['text'] = remplacerSautDeLigne($element->plaintext);
+        $array[$key]['url'] = $url = $element->href;
+    }
  }
 
  $array['logo'] = $img;
